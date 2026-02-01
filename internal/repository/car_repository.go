56a70ac13bbbf323c87a542185f4c8ef50535e62
Abuka13 <abukaozbekbay@gmail.com>
2026-02-01 22:02:50 +0500

    Creating car logic

--- internal/repository/car_repository.go
+++ internal/repository/car_repository.go
@@ -1,1 +1,33 @@
 package repository
+
+import (
+	"database/sql"
+
+	"car-store/internal/model"
+)
+
+type CarRepository struct {
+	db *sql.DB
+}
+
+func NewCarRepository(db *sql.DB) *CarRepository {
+	return &CarRepository{db: db}
+}
+
+func (r *CarRepository) Create(car *model.Car) error {
+	query := `
+		INSERT INTO cars (brand, model, year, price, status, is_auction_only)
+		VALUES ($1, $2, $3, $4, $5, $6)
+		RETURNING id, created_at
+	`
+
+	return r.db.QueryRow(
+		query,
+		car.Brand,
+		car.Model,
+		car.Year,
+		car.Price,
+		car.Status,
+		car.IsAuctionOnly,
+	).Scan(&car.ID, &car.CreatedAt)
+}
--- internal/service/car_service.go
+++ internal/service/car_service.go
@@ -1,1 +1,22 @@
 package service
+
+import "car-store/internal/model"
+
+type CarRepo interface {
+	Create(car *model.Car) error
+}
+
+type CarService struct {
+	repo CarRepo
+}
+
+func NewCarService(repo CarRepo) *CarService {
+	return &CarService{repo: repo}
+}
+
+func (s *CarService) CreateCar(car *model.Car) error {
+	if car.Status == "" {
+		car.Status = "available"
+	}
+	return s.repo.Create(car)
+}
