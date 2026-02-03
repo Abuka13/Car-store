498ec629fdf29594bd0ec260c4cb428a8bb42370
felfir-bit <erkebulan.zhannur@gmail.com>
2026-02-03 18:45:15 +0500

    fixing the problems

--- db/schema.sql
+++ db/schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE cars (
                       year INT NOT NULL,
                       price NUMERIC,
                       status TEXT NOT NULL DEFAULT 'available',
-                      i s_auction_only BOOLEAN DEFAULT FALSE,
+                      is_auction_only BOOLEAN DEFAULT FALSE,
                       created_at TIMESTAMP DEFAULT NOW()
 );
 
