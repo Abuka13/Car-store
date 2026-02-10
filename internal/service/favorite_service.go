6b6426e8a3fd7bdaca3b747d26a321594f905b5d
Abuka13 <abukaozbekbay@gmail.com>
2026-02-11 01:14:03 +0500

    Adding Favorites Logic

--- internal/service/favorite_service.go
+++ internal/service/favorite_service.go
@@ -1,1 +1,43 @@
 package service
+
+import (
+	"errors"
+
+	"car-store/internal/model"
+)
+
+var ErrAlreadyInFavorites = errors.New("car already in favorites")
+
+type FavoriteRepo interface {
+	Add(userID, carID int64) error
+	Remove(userID, carID int64) error
+	Exists(userID, carID int64) (bool, error)
+	GetByUser(userID int64) ([]model.Car, error)
+}
+
+type FavoriteService struct {
+	repo FavoriteRepo
+}
+
+func NewFavoriteService(repo FavoriteRepo) *FavoriteService {
+	return &FavoriteService{repo: repo}
+}
+
+func (s *FavoriteService) AddToFavorites(userID, carID int64) error {
+	exists, err := s.repo.Exists(userID, carID)
+	if err != nil {
+		return err
+	}
+	if exists {
+		return ErrAlreadyInFavorites
+	}
+	return s.repo.Add(userID, carID)
+}
+
+func (s *FavoriteService) RemoveFromFavorites(userID, carID int64) error {
+	return s.repo.Remove(userID, carID)
+}
+
+func (s *FavoriteService) GetMyFavorites(userID int64) ([]model.Car, error) {
+	return s.repo.GetByUser(userID)
+}
